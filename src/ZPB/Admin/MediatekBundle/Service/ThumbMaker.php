48e9cbdd311e607f818bd0a3a6eb8d71051ce2f4
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-08-26 15:29:53 +0200

    sauvegarde

--- src/ZPB/Admin/MediatekBundle/Service/ThumbMaker.php
+++ src/ZPB/Admin/MediatekBundle/Service/ThumbMaker.php
@@ -21,17 +21,17 @@
 namespace ZPB\Admin\MediatekBundle\Service;
 
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use ZPB\Admin\MediatekBundle\Entity\Image;
 
 class ThumbMaker
 {
-    private  $container;
+    private $filesystem;
     private $maxSize = 100;
 
-    public function __construct(ContainerInterface $container)
+    public function __construct(Filesystem $filesystem)
     {
-        $this->container  = $container;
+        $this->filesystem = $filesystem;
     }
 
     /**
@@ -69,12 +69,69 @@ class ThumbMaker
         }
     }
 
-    private function landscape(Image $image){
-
+    private function landscape(Image $image)
+    {
+        $filename = $image->getAbsolutePath();
+        $img = $this->createImage($filename, $image->getMime());
+        $destFilename = $image->getAbsoluteThumbnailPath();
+        if ($image->getWidth() > $this->maxSize) {
+            $ratio = $this->maxSize / $image->getWidth();
+            $newHeight = $image->getHeight() * $ratio;
+            $redim = imagecreatetruecolor($this->maxSize, $newHeight);
+            imagecopyresampled($redim, $img, 0, 0, 0, 0, $this->maxSize, $newHeight, $image->getWidth(), $image->getHeight());
+            $this->save($redim, $destFilename, $image->getMime());
+            imagedestroy($redim);
+        } else {
+            $this->filesystem->copy($filename, $destFilename, true);
+        }
     }
 
     private function portrait(Image $image)
     {
+        $filename = $image->getAbsolutePath();
+        $img = $this->createImage($filename, $image->getMime());
+        $destFilename = $image->getAbsoluteThumbnail();
+        if ($image->getHeight() > $this->maxSize) {
+            $ratio = $this->maxSize / $image->getHeight();
+            $newWidth = $ratio * $image->getWidth();
+            $redim = imagecreatetruecolor($newWidth, $this->maxSize);
+            imagecopyresampled($redim, $img, 0, 0, 0, 0, $newWidth, $this->maxSize, $image->getWidth(), $image->getHeight());
+            $this->save($redim, $destFilename, $image->getMime());
+            imagedestroy($redim);
+        } else {
+            $this->filesystem->copy($filename, $destFilename, true);
+        }
+    }
 
+    private function createImage($file, $mime = 'image/jpeg')
+    {
+        $img = null;
+        switch ($mime) {
+            case 'image/png':
+                $img = imagecreatefrompng($file);
+                break;
+            case 'image/gif':
+                $img = imagecreatefromgif($file);
+                break;
+            case 'image/jpeg':
+                $img = imagecreatefromjpeg($file);
+                break;
+        }
+        return $img;
+    }
+
+    private function save($img, $filename, $mime = 'image/jpeg', $quality = 75)
+    {
+        switch ($mime) {
+            case 'image/png':
+                imagepng($img, $filename);
+                break;
+            case 'image/gif':
+                imagegif($img, $filename);
+                break;
+            case 'image/jpeg':
+                imagejpeg($img, $filename, $quality);
+                break;
+        }
     }
 } 

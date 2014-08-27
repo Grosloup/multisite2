1adbfba1ae8ae90c96f42ebe4d5169e9f4076e14
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-08-28 00:03:21 +0200

    sauvegarde

--- src/ZPB/Admin/CommonBundle/Controller/DownloadsController.php
+++ src/ZPB/Admin/CommonBundle/Controller/DownloadsController.php
@@ -21,11 +21,13 @@
 namespace ZPB\Admin\CommonBundle\Controller;
 
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class DownloadsController extends BaseController
 {
-    public function downloadPdfAction($filename, $format)
+    public function downloadPdfAction($filename, Request $request)
     {
         $pdf = $this->getRepo('')->findOneByFilename($filename);
         if(!$pdf){
@@ -41,12 +43,21 @@ class DownloadsController extends BaseController
         $content = file_get_contents($absPath);
         $response = new Response();
 
+        $response->setLastModified($pdf->getUpdatedAt());
+        $response->headers->set('Cache-Control', 'must-revalidate');
+        $response->setMaxAge(3600);
+        $response->setSharedMaxAge(3600);
+        if($response->isNotModified($request)){
+            return $response;
+        }
+
         $response->headers->set('Content-Type', 'application/pdf');
-        $response->headers->set('Content-Disposition', 'attachment;filename="'.$filename.'".pdf');
+        $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $filename.'.pdf');
+        $response->headers('Content-Length', filesize($absPath));
         $response->setContent($content);
 
-        // header cache ...
+
 
         return $response;
     }
-} 
+}
